d193077b80c2482a78dc19413b8c614adfbd3320
Daniel Pretlove <danielpretlove5@gmail.com>
2022-04-05 20:57:17 +1000

    login form

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,9 @@ const app: Application = express();
 const port = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+
 app.use(routes);
 app.use(PostgresConnector.execute);
+
 app.listen(port);
 
